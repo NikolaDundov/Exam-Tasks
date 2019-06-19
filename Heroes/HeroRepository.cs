ced2806e04e83a2eb3f0c571a4fbe0bccfd362e4
Никола Дундов <40323955+NikolaDundov@users.noreply.github.com>
2019-06-19 23:26:16 +0300

    Add files via upload
    C# Advanced Exam - 24 February 2019 task 3

---
+++ Heroes/HeroRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        public List<Hero> Heroes { get; set; }
+
+        public HeroRepository()
+        {
+            Heroes = new List<Hero>();
+        }
+
+        public void Add(Hero hero)
+        {
+            Heroes.Add(hero);
+        }
+
+        public void Remove(string heroName)
+        {
+            Hero toRemove = Heroes.FirstOrDefault(x => x.Name == heroName);
+            Heroes.Remove(toRemove);
+        }
+
+        public Hero GetHeroWithHighestStrength()
+        {
+            Hero strongest = Heroes.OrderByDescending(x => x.Item.Strength).FirstOrDefault();
+            return strongest;
+        }
+
+        public Hero GetHeroWithHighestAbility()
+        {
+            Hero mostAbility = Heroes.OrderByDescending(x => x.Item.Ability).FirstOrDefault();
+            return mostAbility;
+        }
+
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            Hero mostIntelligent = Heroes.OrderByDescending(x => x.Item.Intelligence).FirstOrDefault();
+            return mostIntelligent;
+        }
+
+        public int Count
+        {
+            get => Heroes.Count;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder text = new StringBuilder();
+
+            foreach (var item in Heroes)
+            {
+                text.AppendLine(item.ToString());
+            }
+
+            return text.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Heroes/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes
+{
+    public class Item
+    {
+        public int Strength { get; set; }
+        public int Ability { get; set; }
+        public int Intelligence { get; set; }
+
+        public Item(int strength, int ability, int intelligence)
+        {
+            Strength = strength;
+            Ability = ability;
+            Intelligence = intelligence;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder text = new StringBuilder();
+            text.AppendLine($"Item:");
+            text.AppendLine($"* Strength: {Strength}");
+            text.AppendLine($"* Ability: {Ability}");
+            text.AppendLine($"* Intelligence: {Intelligence}");
+
+            return text.ToString().TrimEnd();
+        }
+
+    }
+}
