97a13698d88302ee625974774f572e39bbe863de
Никола Дундов <40323955+NikolaDundov@users.noreply.github.com>
2019-06-19 23:29:16 +0300

    Add files via upload
    C# Advanced Exam - 24 February 2019 task 3

---
+++ Repository/Repository/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class Repository
+    {
+        private Dictionary<int, Person> repository;
+        private int id = 0;
+
+        public Repository()
+        {
+            repository = new Dictionary<int, Person>();
+        }
+
+        public void Add(Person person)
+        {
+            repository.Add(id++, person);
+        }
+
+        public Person Get(int id)
+        {
+            Person toGet = null;
+            if (repository.ContainsKey(id))
+            {
+                toGet = repository[id];
+            }
+            return toGet;
+        }
+
+        public bool Update(int id, Person newPerson)
+        {
+            bool updated = false;
+            if (repository.ContainsKey(id))
+            {
+                repository.Remove(id);
+                repository.Add(id , newPerson);
+                updated = true;
+            }
+            return updated;
+        }
+
+        public bool Delete(int id)
+        {
+            bool deleted = false;
+            if (repository.ContainsKey(id))
+            {
+                deleted = true;
+                repository.Remove(id);
+            }
+            return deleted;
+        }
+
+        public int Count
+        {
+            get => repository.Count();
+        }
+    }
+}
---
+++ Repository/Repository/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Repository
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Person firstPerson = new Person("George", 10, new DateTime(2009, 05, 04));
+            Person secondPerson = new Person("Peter", 5, new DateTime(2014, 05, 24));
+            Person thirdPerson = new Person("Pesho", 23, new DateTime(2012, 05, 22));
+
+            Repository myRepository = new Repository();
+            myRepository.Add(firstPerson);
+            myRepository.Add(secondPerson);
+
+           
+
+            Console.WriteLine(myRepository.Update(0, thirdPerson));
+            Console.WriteLine(myRepository.Count);
+            Console.WriteLine(myRepository.Update(0, secondPerson));
+            Console.WriteLine(myRepository.Delete(0));
+        }
+    }
+}
